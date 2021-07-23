1973f27249e5b761a7379f5c4c274666d9f16903
Faisal Mahamood <faisalmahamood15@gmail.com>
2021-07-24 04:58:36 +0530

    'corsedit'

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ app.use('/files', require('./routes/show'));
 app.use('/api/files', require('./routes/files'));
 app.use('/files/download', require('./routes/download'));
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 
