f9bfdf0bc2fc0c7f5193903a9d8ef55d34f254e7
Faisal Mahamood <faisalmahamood15@gmail.com>
2021-07-23 21:35:29 +0530

    'changes'

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ connectDB();
 app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-
+app.use('/files', require('./routes/show'));
 app.use('/api/files', require('./routes/files'));
 app.use('/files/download', require('./routes/download'));
-app.use('/files', require('./routes/show'));
+
 
 
 
