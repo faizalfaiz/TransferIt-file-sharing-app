1b1216f9439ba892954f4190d0229a26f66d0d61
Faisal Mahamood <faisalmahamood15@gmail.com>
2021-07-23 09:42:15 +0530

    'cors1'

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 // Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
 
-app.use(cors(corsOptions));
+
 app.use(express.static('public'));
 require('./config/db');
 connectDB();
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+  
+app.use(cors(corsOptions));
 app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
